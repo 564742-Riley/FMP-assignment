e57981eef5fd76640e1f9b7d4150dfd84c1ffd63
564742-Riley <564742@escg.ac.uk>
2026-05-05 12:13:26 +0100

    start UI

---
+++ Assets/Scripts/Main Menu/ButtonScripts.cs
@@ -0,0 +1,55 @@
+using TMPro;
+
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class ButtonScripts : MonoBehaviour
+{
+    public TMP_Text buttonText;
+
+    public void BackLevelSelect()
+    {
+        buttonText.text = "x";
+        SceneManager.LoadScene("LevelSelect");
+
+    }
+    public void QuitButton()
+    {
+
+        SceneManager.LoadScene("FrontEnd");
+
+    }
+    public void PlayButton()
+    {
+        buttonText.text = "Loading...";
+        SceneManager.LoadScene("LevelSelect");
+
+    }
+    
+
+    public void Level1()
+    {
+        buttonText.text = "Loading...";
+        SceneManager.LoadScene("Level1");
+
+    }
+    
+    public void QuitGame()
+    {
+        Application.Quit();
+    }
+
+
+    public void PlaySFX()
+    {
+       // AudioManager.instance.Play("SFX");
+    }
+
+    public void MusicMute(bool mute)
+    {
+        //AudioManager.instance.musicMute = mute;
+    }
+
+    
+}
+
